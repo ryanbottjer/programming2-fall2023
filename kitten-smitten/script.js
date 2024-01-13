992293539d1db4e6937c83ea82012b73df96b5c7
Ryan Bottjer <ryanbottjer@MacBook-Pro-6.local>
2024-01-13 12:11:03 -0600

    almost final

--- kitten-smitten/script.js
+++ kitten-smitten/script.js
@@ -1,15 +1,34 @@
-<script>
-  // Get the elements by their ID
-  var popupLink = document.getElementById("popupLink");
-  var popupWindow = document.getElementById("popupWindow");
-  var closeButton = document.getElementById("closeButton");
-  // Show the pop-up window when the link is clicked
-  popupLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    popupWindow.style.display = "block";
-  });
-  // Hide the pop-up window when the close button is clicked
-  closeButton.addEventListener("click", function() {
-    popupWindow.style.display = "none";
-  });
-</script>  
+const publicKey = "Qd5iBPTV-SiEbzciD";
+const serviceID = "service_qepq1ed";
+const templateID = "template_v8ydk7q";
+
+emailjs.init(publicKey);
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(e){
+    e.preventDefault()
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var subject = document.getElementById('subject');
+    var message = document.getElementById('message');
+    const inputFields = {
+        to_name: "Deborah",
+        from_name: name.value,
+        reply_to: email.value,
+        message: message.value,
+        subject: subject.value
+    }
+
+emailjs.send(serviceID, templateID, inputFields)
+.then(()=> {
+    btn.innerText = "Message Sent Successfully.";
+    name.value = "";
+    email.value = "";
+    message.value = "";
+    subject.value = "";
+    }, (error) => {
+        console.log(error);
+        submitBtn.innterText = "Something went wrong...";
+    });
+
+});
